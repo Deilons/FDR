1e27b2a09e99d0e717b80eb0cf87fce69ef585a5
Deilons <apolo4748@gmail.com>
2024-10-07 12:43:07 -0500

    fix: Guest Put

--- Controllers/V1/Guest/GuestPUTControllers.cs
+++ Controllers/V1/Guest/GuestPUTControllers.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FDR.DTOs;
+using FDR.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FDR.Controllers.V1.Guest
@@ -10,8 +12,19 @@ namespace FDR.Controllers.V1.Guest
     [Route("api/v1/guest")]
     [ApiExplorerSettings(GroupName = "v1")]
     [Tags("Guest")]
-    public class GuestPUTControllers : ControllerBase
+    public class GuestPUTControllers : GuestController
     {
-        
+        public GuestPUTControllers(IGuestRepository guestRepository) : base(guestRepository)
+        {
+        }
+
+
+        [HttpPut ("{id}")]
+        public async Task<ActionResult> Update(int id, GuestDTO guestDTO)
+        {
+            await services.Update(id, guestDTO);
+            return NoContent();
+        }
     }
+        
 }
