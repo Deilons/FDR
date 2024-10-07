382cd6ce413b694f27d6864f970c3d620a6a9328
Deilons <apolo4748@gmail.com>
2024-10-07 07:38:47 -0500

    Feat: Booking DTO

---
+++ DTOs/BookingDTO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FDR.DTOs;
+
+public class BookingDTO
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public int GuestId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public double TotalCost { get; set; }
+}
